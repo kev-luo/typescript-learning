086383268bcf5ffe36e79d28794de21b2e1d4ead
Kevin Luo <kvn.luo@gmail.com>
2020-12-18 16:00:37 -0500

    function signatures

--- public/5-functionSignatures.js
+++ public/5-functionSignatures.js
@@ -1,3 +1,25 @@
 "use strict";
 // function signatures
 // =======================================================
+// NOTE: EXAMPLE ONE
+// function signature
+var greeting;
+// matches fxn signature
+greeting = function (name, greeting) {
+    console.log(name + " says " + greeting);
+};
+// NOTE: EXAMPLE TWO
+var calc;
+// since we defined calc to return a number, we have to add an else statement. Otherwise, if operator doesn't equal "add" then it would return undefined which doesn't match the return type in our fxn signature
+calc = function (num1, num2, operator) {
+    if (operator === "add") {
+        return num1 + num2;
+    }
+    else {
+        return num1 - num2;
+    }
+};
+var playerDetails;
+playerDetails = function (player) {
+    console.log(player.name + " is " + player.age + " years old");
+};
--- src/5-functionSignatures.ts
+++ src/5-functionSignatures.ts
@@ -1,2 +1,31 @@
 // function signatures
 // =======================================================
+// NOTE: EXAMPLE ONE
+// function signature
+let greeting: (a: string, b: string) => void;
+
+// matches fxn signature
+greeting = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
+
+// NOTE: EXAMPLE TWO
+let calc: (a: number, b: number, c: string) => number;
+
+// since we defined calc to return a number, we have to add an else statement. Otherwise, if operator doesn't equal "add" then it would return undefined which doesn't match the return type in our fxn signature
+calc = (num1: number, num2: number, operator: string) => {
+  if(operator === "add") {
+    return num1 + num2;
+  } else {
+    return num1 - num2;
+  }
+}
+
+// NOTE: EXAMPLE THREE
+type Player = {name: string, age: number}
+
+let playerDetails: (obj: Player) => void;
+
+playerDetails = (player: Player) => {
+  console.log(`${player.name} is ${player.age} years old`)
+}
