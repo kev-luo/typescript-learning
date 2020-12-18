f78ba7f950c528b50a773cc3764eb266e50b2b4e
Kevin Luo <kvn.luo@gmail.com>
2020-12-18 15:42:00 -0500

    function and types

--- public/sandbox.js
+++ public/sandbox.js
@@ -38,3 +38,33 @@ playerOne = { name: "messi", age: 32 };
 playerOne = [];
 var playerTwo;
 playerTwo = { name: 'kane', team: 'spurs', age: 28 };
+// Function basics
+// =======================================================
+var greet;
+greet = function () {
+    console.log('i greet things');
+};
+// b: number = 10 indicates 10 is the default value if no argument for b is passed
+// c?: number | string indicates c is an optional argument
+var add = function (a, b, c) {
+    if (b === void 0) { b = 10; }
+    var result = a + b;
+    console.log(result);
+    return result;
+};
+add(5);
+add(5, 3);
+var minus = function (a, b) {
+    return a + b;
+};
+// type of result is inferred from function result
+var result = minus(10, 7);
+// explicitly state result type
+var multiply = function (a, b) {
+    return a * b;
+};
+// no return statement in function returns void
+// when compiled to javascript this gets read as undefined
+var noReturn = function (a, b) {
+    console.log(a, b);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -53,3 +53,42 @@ let playerTwo: {
 }
 
 playerTwo = {name: 'kane', team: 'spurs', age: 28}
+
+
+// Function basics
+// =======================================================
+let greet: Function;
+
+greet = () => {
+  console.log('i greet things')
+}
+
+// b: number = 10 indicates 10 is the default value if no argument for b is passed
+// c?: number | string indicates c is an optional argument
+const add = (a: number, b: number = 10, c?: number | string) => {
+  const result = a + b;
+  console.log(result);
+  return result;
+}
+
+add(5);
+add(5,3);
+
+const minus = (a: number, b: number) => {
+  return a + b;
+}
+
+// type of result is inferred from function result
+let result = minus(10,7);
+
+// explicitly state result type
+const multiply = (a: number, b: number): number => {
+  return a * b;
+}
+
+// no return statement in function returns void
+// when compiled to javascript this gets read as undefined
+const noReturn = (a: number, b: number): void => {
+  console.log(a, b);
+}
+
