907d5977b8e73c9ed77d1f1d29360c2cb6588b71
Kevin Luo <kvn.luo@gmail.com>
2020-12-18 14:43:06 -0500

    type basics

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+var character = 'mario';
+// explicitly define the diameter argument as a number
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(4));
+// when defining arrays, the values in the array will be inferred based on what the initial values are. In this case typescript will only allow numbers to be added to this array
+var numbers = [10, 20, 30, 40];
+numbers[1] = 23;
+// numbers.push("hello"); // Argument of type 'string' is not assignable to parameter of type 'number'
+var player = {
+    name: "messi",
+    team: "barcelona",
+    age: 32
+};
+player.name = "ronaldo";
+player.team = "juventus";
+// player.age = '37'; // string not assignable to number
+player['skills'] = ['jumping', 'dribbling'];
+console.log(player);
+// explicit types
+// ==================================================
+// initializes players variable and tells typescript that it will be an array of strings
+// NOTE: this doesn't actually assign players equal to an empty array
+var players;
+// initializes mixed variables and tells typescript that it will be an array of items that will be one of three specified types
+var mixed = [];
+mixed.push('hello');
+mixed.push(23);
+mixed.push(true);
---
+++ sandbox.ts
@@ -0,0 +1,38 @@
+const character = 'mario';
+
+// explicitly define the diameter argument as a number
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+
+console.log(circ(4));
+
+// when defining arrays, the values in the array will be inferred based on what the initial values are. In this case typescript will only allow numbers to be added to this array
+let numbers = [10, 20, 30, 40];
+
+numbers[1] = 23
+// numbers.push("hello"); // Argument of type 'string' is not assignable to parameter of type 'number'
+
+let player = {
+  name: "messi",
+  team: "barcelona",
+  age: 32
+}
+
+player.name = "ronaldo";
+player.team = "juventus";
+// player.age = '37'; // string not assignable to number
+player['skills'] = ['jumping', 'dribbling']
+console.log(player);
+
+// explicit types
+// ==================================================
+// initializes players variable and tells typescript that it will be an array of strings
+// NOTE: this doesn't actually assign players equal to an empty array
+let players: string[];
+
+// initializes mixed variables and tells typescript that it will be an array of items that will be one of three specified types
+let mixed: (string | number | boolean)[] = []
+mixed.push('hello');
+mixed.push(23);
+mixed.push(true);
